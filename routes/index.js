ef22ca4afb9b4ecbe43d1045d749c11cde7e5cb0
Mohsen Alowayed <mohsen.alowayed@gmail.com>
2017-05-31 17:38:25 +0300

    Countdown added

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,14 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page */
+/* GET homepage */
 router.get('/', function(req, res, next) {
   res.render('index');
+});
+
+/* GET countdown */
+router.get('/countdown', function(req, res, next) {
+  res.render('countdown.html');
 });
 
 module.exports = router;
